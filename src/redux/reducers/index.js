5f9b4f3bc664fd14a6f53dbf37a68f713b91c2ab
jeremie_giffard <e132784f@gmail.com>
2022-05-10 10:47:40 +0200

    thunks build

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import courses from "./courseReducer";
+import authors from "./authorReducer";
 
 const rootReducter = combineReducers({
 	// courses: courses          //right and left are the same
 	courses,
+	authors,
 });
 
 export default rootReducter;
